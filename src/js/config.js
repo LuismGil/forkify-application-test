af5f044bd056be7b91363702cfc60d13d0fc876e
LuisM <luism6977@gmail.com>
2020-12-01 07:31:39 -0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4bef5861-274c-44d4-a8e6-53c972816f9b';
+export const MODAL_CLOSE_SEC = 2.5;
